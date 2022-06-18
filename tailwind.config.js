42a0692ca11d31bb56786efe7a01a52708c4c38c
Wingrammer <wilpar49@hotmail.com>
2022-06-18 21:10:00 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**.html"
+  ],
+  theme: {
+    colors:{
+      blueHead:'#1B91BF',
+      goldHead:'#BFA65A',
+      orangeHead:'#F2A71B',
+      black:'#000000',
+      white:'#fff',
+      blacka:'#262626',
+      transparent:'transparent'
+    },
+    extend: {},
+    backdropBlur: {
+      flou: '20px',
+    }
+  },
+  plugins: [],
+}
